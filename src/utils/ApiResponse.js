7c7518c63f2c265c11a6e787183deb5128fc0367
Roshan Arora <roshanarora.8905@gmail.com>
2026-08-15 12:49:15 +0530

    learned about utils

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,7 @@
+class ApiResponse {
+    constructor(statusCode, message = "Success", data) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+        this.success = statusCode < 400;
+    }
